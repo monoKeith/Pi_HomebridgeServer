cb326708509d22af87ed64ac6bd5b896aa7d1ac5
monoKeith <hgyixg@gmail.com>
2021-02-16 04:33:39 +0000

    Wrapped UI into UI_Class

--- UI.py
+++ UI.py
@@ -5,44 +5,43 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
-# Initialize display
-DISPLAY = Adafruit_SSD1306.SSD1306_128_64(rst=24)
-DISPLAY.begin()
-clear_screen()
 
-# Fonts
-FONT    = ImageFont.truetype("Fonts/PTMono.ttc", 23)
-FONT_TC = ImageFont.truetype("Fonts/PingFang.ttc", 55)
-EMOJI   = ImageFont.truetype("Fonts/AppleEmoji.ttc", 48)
+class UI_class:
+    
+    # Splash screen
+    def splash(self):
+        splash = Image.new('1', (self.WIDTH, self.HEIGHT))
+        draw = ImageDraw.Draw(splash)
+        draw.text((10,0), "嘿嘿", font=self.FONT_TC, fill=256)
+        self.update(splash)
 
-WIDTH  = DISPLAY.width
-HEIGHT = DISPLAY.height
+    # Print provided image to screen
+    def update(self, image):
+        self.DISPLAY.image(image)
+        self.DISPLAY.display()
 
+    # Blank screen
+    def clear_screen(self):
+        self.DISPLAY.clear()
+        self.DISPLAY.display()
 
-# Splash screen
-splash = Image.new('1', (WIDTH, HEIGHT))
-draw = ImageDraw.Draw(splash)
-draw.text((10,0), "加油", font=FONT_TC, fill=256)
-
-
-def update(image):
-    DISPLAY.image(image)
-    DISPLAY.display()
-
-
-def clear_screen():
-    DISPLAY.clear()
-    DISPLAY.display()
+    
+    def print_text(self, text):
+        image = Image.new('1', (self.WIDTH, self.HEIGHT))
+        draw = ImageDraw.Draw(image)
+        draw.text((10,0), text, font=self.FONT, fill=256)
+        self.update(image)
 
 
-def print_text(text):
-    image = Image.new('1', (WIDTH, HEIGHT))
-    draw = ImageDraw.Draw(image)
-    draw.text((10,0), text, font=FONT, fill=256)
-    update(image)
+    def __init__(self):
+        # Initialize display
+        self.DISPLAY = Adafruit_SSD1306.SSD1306_128_64(rst=24)
+        self.DISPLAY.begin()
+        self.clear_screen()
+        self.WIDTH  = self.DISPLAY.width
+        self.HEIGHT = self.DISPLAY.height
+        # Define fonts
+        self.FONT    = ImageFont.truetype("Fonts/PTMono.ttc", 23)
+        self.FONT_TC = ImageFont.truetype("Fonts/PingFang.ttc", 55)
+        self.EMOJI   = ImageFont.truetype("Fonts/AppleEmoji.ttc", 48)
 
-
-
-#update(splash)
-
-update(splash.convert('1'))
--- daemon.py
+++ daemon.py
@@ -1,13 +1,16 @@
 import time
 import IO
-import UI as UI_CTL
+import UI
 import switchServer as SW_SERVER
 
 
 # Setup
+UI_CTL = UI.UI_class()
 IO_CTL = IO.IO_class()
 SW_SERVER.IO_CTL = IO_CTL
 
+# Splash screen
+UI_CTL.splash()
 
 # Run flask server.
 SW_SERVER.app.run(host="localhost", port=5001, debug=False)
